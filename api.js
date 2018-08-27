c836c5b003269ac52e93b4eaa1c51572a2070aac
Henrik Åberg <henrik@twero.com>
2018-08-27 16:06:51 +0200

    Making media.viewAttachment dynamic

--- api.js
+++ api.js
@@ -686,7 +686,17 @@
 
         return this.get('/media/rating/', media, callback);
       },
-      viewAttachment  : [GENERATE_GET_APPEND_PARAM1_TO_URL, '/media/attachment/'],
+      viewAttachment  : function (data, callback) {
+        // Legacy-support, media is the ID and not the object
+        if (typeof data === 'number') {
+          const container = data;
+          data = {
+            postId : container,
+          };
+        }
+
+        return this.get('/media/attachment', data, callback);
+      },
       viewAttachments : function (data, callback) {
         return this.get('/media/attachments', data, callback);
       },
